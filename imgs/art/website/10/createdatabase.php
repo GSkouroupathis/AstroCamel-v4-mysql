45f8526ea4e34dd9dcb95ec6a2847c4e2677c153
George Skouroupathis <george_skouroupathis@hotmail.com>
2012-12-14 05:57:23 +0000

    Update imgs/art/website/10/createdatabase.php

--- imgs/art/website/10/createdatabase.php
+++ imgs/art/website/10/createdatabase.php
@@ -1,7 +1,7 @@
 <?
-$user="a2207475_1";
-$password="a2207475_2";
-$database="a2207475_db";
+$user="f75_1";
+$password="af";
+$database="a220f_db";
 mysql_connect("mysql2.000webhost.com",$user,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 $query="CREATE TABLE msgs (id int(6) NOT NULL auto_increment,first varchar(20),last varchar(30),email varchar(50),tel varchar(15),comments text,date varchar(16),PRIMARY KEY (id))";
